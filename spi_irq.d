34e137d9ef98945517abe65f6e222afd9cd69445
Eman Megahed <50622434+eman1233@users.noreply.github.com>
2020-08-25 19:23:31 +0200

    Add files via upload

---
+++ spi_irq.d
@@ -0,0 +1,3 @@
+./objects/spi_irq.o: Spi_Irq.c Spi_MemMap.h Spi.h Spi_Cfg.h Std_Types.h \
+  Platform_Types.h Compiler.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ spi_pbcfg.d
@@ -0,0 +1,3 @@
+./objects/spi_pbcfg.o: Spi_PBcfg.c Spi_MemMap.h Spi.h Spi_Cfg.h \
+  Std_Types.h Platform_Types.h Compiler.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
